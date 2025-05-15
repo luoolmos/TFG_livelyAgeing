13c19974cbb6d9936e382c1218ee476d174806f0
luoolmos <146450799+luoolmos@users.noreply.github.com>
2025-05-15 11:11:52 +0200

    manejoVariables remoteServer

--- migration_Samsung/routes/sleep.js
+++ migration_Samsung/routes/sleep.js
@@ -10,18 +10,18 @@ router.get('/sensors/save-sleep', async (req, res) => {
     try {
         const access_token = process.env.ACCESS_TOKEN;
         const source = constants.SAMSUNG_GALAXY_WATCH_4;
-        const {user_id, last_sync_date} = await getUserDeviceInfo(source);
-        console.log('user_id:', user_id);
-        console.log('last_sync_date:', last_sync_date);
-        last_sync_date = '2025-05-05';
+        const {userId, lastSyncDate, userDeviceId} = await getUserDeviceInfo(source);
+        console.log('user_id:', userId);
+        console.log('last_sync_date:', lastSyncDate);
+        lastSyncDate = '2025-05-05';
         const response = await makeAuthenticatedRequest(
-            `https://api.fitbit.com/1/user/-/sleep/date/${last_sync_date}.json`,
+            `https://api.fitbit.com/1/user/-/sleep/date/${lastSyncDate}.json`,
             access_token
         );
         console.log('response:', response.data);
         res.json(response.data);
 
-        await getSleepAndSave(user_id, access_token, last_sync_date);
+        await getSleepAndSave(userId, access_token, lastSyncDate);
         res.send("Datos de sueño guardados en la base de datos.");
     } catch (error) {
         console.error('Error in save-sleep endpoint:', error);
